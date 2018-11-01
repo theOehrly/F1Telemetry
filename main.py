51ff749b505920cc838fb199e306968531ce5df6
theOehrly <phi.schaefer@mailbox.org>
2018-11-01 18:51:50 +0100

    changed to match new usage of recognition.do_recognition

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from videosource import VideoSource
 from ui import Application
 from recognition import do_regocgnition
 
-infile = 'results/mex_q2/ver_alo_q2.mp4'
+infile = 'results/mex_q3/mex_q3_ver_ham.mp4'
 uid = 'alo_q2'
 
 videosource = VideoSource(infile)
 # videosource = VideoSource('testfiles/test1.mp4')
-outfile = 'results/ver_q2_ref.csv'
+outfile = 'results/ver_q3_ref.csv'
 
 timing_data = [0, 0, 0]
 
