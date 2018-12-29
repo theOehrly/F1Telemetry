5b875392e50921769dc6facf705851221fdb946b
theOehrly <phi.schaefer@mailbox.org>
2018-12-29 21:02:23 +0100

    remove usage of old ui

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 from videosource import VideoSource
-from ui import Application
-# from recognition import do_regocgnition
+from recognition import do_regocgnition
 
-infile = 'C:/users/phisc/sonstige/test1.mp4'
-uid = 'alo_q2'
+infile = 'D:\\Dateien\\Projekte\\F1Telemetry\\Races\\2018\\Brasilien\\vettelq3.mp4'
+uid = 'vet_q3'
 
 videosource = VideoSource(infile)
 # videosource = VideoSource('testfiles/test1.mp4')
-outfile = 'results/ver_q3_ref.csv'
+outfile = 'D:\\Dateien\\Projekte\\F1Telemetry\\Races\\2018\\Brasilien\\vettelq3.csv'
 
 timing_data = [0, 0, 0]
 selection = [81, 359, 42]
-
-app = Application(videosource, timing_data, selection)
-
-print("[INFO] starting...")
-app.mainloop()
 
 print(selection)
 
 # do_regocgnition(videosource, timing_data, selection, outfile, uid)
 
-videosource.release()  # release web camera
+videosource.release()
