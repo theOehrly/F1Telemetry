22e5f366de4e745828a59055c0f35562785b1b9d
theOehrly <phi.schaefer@mailbox.org>
2018-12-29 17:01:05 +0100

    Switching UI from TKinter to PyQt5 - Part 1 Video Player, Playback speed, Video Progress Bar, Seeking, Frame by Frame

---
+++ qtmainui.py
@@ -0,0 +1,138 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
+
+from ui_mainwindow import Ui_MainWindow
+import sys
+from videosource import VideoSource
+
+
+class MainUI(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # self.ui = ui_mainwindow.Ui_MainWindow()
+        self.setupUi(self)
+        self.playing = False
+        self.was_playing = False
+
+        self.frame_timer = QTimer()
+        self.frame_timer.timeout.connect(self.load_next_frame)
+
+        self.videosource = VideoSource('D:\\Dateien\\Projekte\\F1Telemetry\\Races\\2018\\Brasilien\\vettelq3.mp4')
+
+        self.init_ui()
+
+    def init_ui(self):
+        # playpause button
+        self.btn_playpause.clicked.connect(self.playpause)
+
+        # playback speed slider
+        self.slider_speed.valueChanged.connect(lambda: self.change_playback_speed(self.slider_speed.value()))
+
+        # video progress slider
+        str_total = self.format_frame_time(self.videosource.total_frames, self.videosource.duration)
+        self.lbl_markend.setText(str_total)
+        self.lbl_playbacktotal.setText(str_total)
+
+        self.slider_videopos.setMaximum(self.videosource.total_frames)
+        self.slider_videopos.sliderPressed.connect(self.slider_videopos_pressed)
+        self.slider_videopos.sliderReleased.connect(self.slider_videopos_released)
+        self.slider_videopos.sliderMoved.connect(self.slider_videopos_value_changed)
+
+        # frame by frame buttons
+        self.btn_previousframe.clicked.connect(self.previous_frame)
+        self.btn_nextframe.clicked.connect(self.next_frame)
+
+        self.load_next_frame()
+        self.show()
+
+    def load_next_frame(self, set_slider=True):
+        frame, frame_pos, frame_duration = self.videosource.get_frame()
+        img = QImage(frame.data, frame.shape[1], frame.shape[0], frame.shape[1]*3, QImage.Format_RGB888)
+        self.lbl_display.setPixmap(QPixmap(img))
+
+        str_current = self.format_frame_time(frame_pos, frame_pos/self.videosource.source_fps)
+        self.lbl_playbackpos.setText(str_current)
+        if set_slider:
+            self.slider_videopos.setValue(frame_pos)
+
+    def slider_videopos_pressed(self):
+        self.was_playing = self.playing
+        self.stop_playback()
+
+    def slider_videopos_released(self):
+        if self.was_playing:
+            self.start_playback()
+
+    def slider_videopos_value_changed(self):
+        self.seek_to(self.slider_videopos.value())
+
+    def start_playback(self):
+        self.frame_timer.start(self.videosource.playback_frame_duration)
+        self.playing = True
+
+    def stop_playback(self):
+        self.frame_timer.stop()
+        self.playing = False
+
+    def playpause(self):
+        if self.playing:
+            self.stop_playback()
+            self.enable_frame_by_frame()
+        else:
+            self.change_playback_speed(self.slider_speed.value())
+            self.start_playback()
+            self.disable_frame_by_frame()
+
+    def enable_frame_by_frame(self):
+        self.btn_previousframe.setDisabled(0)
+        self.btn_nextframe.setDisabled(0)
+        self.btn_markstart.setDisabled(0)
+        self.btn_markend.setDisabled(0)
+        self.btn_markzero.setDisabled(0)
+
+    def disable_frame_by_frame(self):
+        self.btn_previousframe.setDisabled(1)
+        self.btn_nextframe.setDisabled(1)
+        self.btn_markstart.setDisabled(1)
+        self.btn_markend.setDisabled(1)
+        self.btn_markzero.setDisabled(1)
+
+    def change_playback_speed(self, speed):
+        speed /= 10
+        self.lbl_playbackspeed.setText(str(speed))
+        if speed == 0:
+            self.stop_playback()
+            # self.videosource.playback_direction = 1
+            self.enable_frame_by_frame()
+        elif speed != 0 and not self.playing:
+            self.disable_frame_by_frame()
+            self.videosource.set_playback_speed(speed)
+            # start playback
+            self.start_playback()
+        else:
+            self.videosource.set_playback_speed(speed)
+            self.frame_timer.setInterval(self.videosource.playback_frame_duration)
+
+    def seek_to(self, frame):
+        self.videosource.seek_to(int(frame))
+        self.load_next_frame(set_slider=False)
+
+    def next_frame(self):
+        self.videosource.set_playback_speed(1)
+        self.load_next_frame()
+
+    def previous_frame(self):
+        self.videosource.set_playback_speed(-1)
+        self.load_next_frame()
+
+    @staticmethod
+    def format_frame_time(frames, seconds):
+        return "{:d}f / {:.2f}s".format(int(frames), seconds)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainui = MainUI()
+    sys.exit(app.exec())
