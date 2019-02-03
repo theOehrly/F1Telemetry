2377d6e0313293c419aa3fe5bde84ef1e242fc5c
theOehrly <phi.schaefer@mailbox.org>
2019-02-03 21:47:40 +0100

    improved appplication rescaling

--- ui/qtmainui.py
+++ ui/qtmainui.py
@@ -10,14 +10,20 @@ class MainUI(QMainWindow, Ui_MainWindow):
         super().__init__()
 
         self.setupUi(self)
-        self.videoplayer = VideoPlayerWidget(self.tab_video, selection_data, videofile)
+
+        # add videoplayer widget into prepared placeholder
+        self.videoplayer = VideoPlayerWidget(self.playercontainer, selection_data, videofile)
+        self.playercontainer.setMinimumSize(self.videoplayer.minimumSize())
+
         self.init_ui()
 
     def init_ui(self):
-        self.tab_video.installEventFilter(self)
+        # install eventfilter on playercontainer
+        self.playercontainer.installEventFilter(self)
         self.show()
 
     def eventFilter(self, _object, _event):
         if _event.type() == QResizeEvent.Resize:
+            # resize videoplayer widget
             self.videoplayer.resize(_event.size())
         return False
